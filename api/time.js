4145a64c75622b2aeea12c00b2cfc21c341e49ed
limfff <reallimff@gmail.com>
2025-05-09 00:57:28 +0800

    time.js

--- api/time.js
+++ api/time.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 export default async function handler(req, res) {
   const response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Shanghai");
   const data = await response.json();
