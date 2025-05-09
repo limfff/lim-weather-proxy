d1bde3274a28bf2261f38e0958d405eabf1d059b
limfff <reallimff@gmail.com>
2025-05-09 18:04:57 +0800

    Update minutely-rain.js

--- api/minutely-rain.js
+++ api/minutely-rain.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { location } = req.query;
   const key = process.env.WEATHER_KEY;
-  const url = `https://devapi.qweather.com/v7/minutely/5m?location=${location}&key=${key}`;
+  const url = `https://mr487qncjq.re.qweatherapi.com/v7/minutely/5m?location=${location}&key=${key}`;
   const response = await fetch(url);
   const data = await response.json();
   res.status(200).json(data);
