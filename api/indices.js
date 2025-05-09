2d4cbde6cb51354aee12a169e2ec31b52501f7e0
limfff <reallimff@gmail.com>
2025-05-09 18:06:00 +0800

    Update indices.js

--- api/indices.js
+++ api/indices.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { location, type } = req.query;
   const key = process.env.WEATHER_KEY;
-  const url = `https://devapi.qweather.com/v7/indices/1d?location=${location}&type=${type || "3,8"}&key=${key}`;
+  const url = `https://mr487qncjq.re.qweatherapi.com/v7/indices/1d?location=${location}&type=${type}&key=${key}`;
   const response = await fetch(url);
   const data = await response.json();
   res.status(200).json(data);
